13fdfc1ea9bb7a1fbe611b97919c5d26fb69d9fa
safiaali926 <163167812+safiaali926@users.noreply.github.com>
2024-09-14 23:28:17 +0500

    Add files via upload

---
+++ script.ts
@@ -0,0 +1,42 @@
+// get referances to the form  and display area
+
+const form = document.getElementById('resume-form') as HTMLFormElement
+const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement
+
+//handle form submission
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();  // prevent page reload
+
+    //collect input values
+    const name = (document.getElementById('name') as HTMLInputElement).value
+    const email = (document.getElementById('email') as HTMLInputElement).value
+    const phone = (document.getElementById('phone') as HTMLInputElement).value
+    const education = (document.getElementById('education') as HTMLSelectElement).value
+    const experience = (document.getElementById('experience') as HTMLSelectElement).value
+    const skills = (document.getElementById('skills') as HTMLTextAreaElement).value
+
+    //generate the resume content dynamically
+    const resumeHTML = `
+    <h2><b>Editable Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b><span contenteditable="true">${name}</span></p>
+    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+    <p><b>Phone:</b><span contenteditable="true">${phone}</span></p>
+    
+    <h3>Education</h3>
+    <p contenteditable="true">${education}</p>
+    
+    <h3>Experience</h3>
+    <p contenteditable="true">${experience}</p>
+    
+    <h3>Skills</h3>
+    <p contenteditable="true">${skills}</p>`
+
+    //display the generate resume
+
+    if(resumeDisplayElement){
+        resumeDisplayElement.innerHTML = resumeHTML;
+    }else{
+        console.error('The resume display element is missing.');
+    }
+})
